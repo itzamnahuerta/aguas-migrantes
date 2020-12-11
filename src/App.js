8ddfeace9f7a18f39fe0f3a65c5996d27b923da1
Itzamna Huerta <_@itzamna-huerta.local>
2020-12-11 14:18:56 -0600

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+// current main pages
+
+import LandingPage from './components/LandingPage';
+import HomeScreen  from './components/HomeScreen';
+import AboutScreen from './components/AboutScreen';
+import NavBar from './components/NavBar';
+import SidebarMenu from './components/SidebarMenu';
+import Shop from './components/Shop';
+
+import OjoDeAguaFilmFestival2020Screen from './components/OjoDeAguaFilmFestival2020/OjoDeAguaFilmFestival2020Screen.jsx';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [landingPage, setLandingPage] = useState(true);
+
+  const toggleLandingPage = () => setLandingPage(!landingPage);
+
+  if(landingPage === true) {
+    return(
+      <React.Fragment> 
+        <LandingPage
+          landingPage={landingPage}
+          toggleLandingPage={toggleLandingPage}
+        />
+      </React.Fragment> 
+    )
+  } else {
+    return (
+      <div className="App">
+        <Router>
+          <NavBar />
+          <Switch>
+          <Route path="/ojo-agua-film-festival-2020">
+              <OjoDeAguaFilmFestival2020Screen />
+            </Route>
+            <Route path="/about">
+              <AboutScreen />
+            </Route>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
