91b7f457b1d0d341d05e3657e52b30a0563adb2b
Itzamna Huerta <_@itzamna-huerta.local>
2020-12-11 14:27:51 -0600

    import section three

--- src/components/OjoDeAguaFilmFestival2020/ParallaxBody.jsx
+++ src/components/OjoDeAguaFilmFestival2020/ParallaxBody.jsx
@@ -7,13 +7,14 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 // page sections 
 import SectionOne from './SectionOne';
 import SectionTwo from './SectionTwo';
-
+import SectionThree from './SectionThree';
 
 const ParallaxBody = () => {
   return(
     <ParallaxProvider>
       <SectionOne/>
       <SectionTwo/>
+      <SectionThree/>
   </ParallaxProvider>
   )
 };
