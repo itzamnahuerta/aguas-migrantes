be5a0f8565b7eb785e0bd5acbe55b32703e19ccf
Itzamna Huerta <_@itzamna-huerta.local>
2020-12-11 14:16:19 -0600

    empty functional componentt

---
+++ src/components/OjoDeAguaFilmFestival2020/SectionThree.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionThree = () => {
+  return(
+    <div className="section-2-container live-stream-container"> 
+  
+    </div>     
+  )
+};
+
+export default SectionThree;
