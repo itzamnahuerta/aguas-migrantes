d0ca6ac74502ee7bb3f891d016083b4a5b3ed0d3
Itzamna Huerta <_@itzamna-huerta.local>
2020-12-15 18:10:12 -0600

    toggling 3 buttons but having issues witht rendering content onclick

--- src/components/OjoDeAguaFilmFestival2020/SectionThree.jsx
+++ src/components/OjoDeAguaFilmFestival2020/SectionThree.jsx
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState }  from 'react';
+
+
 
 const SectionThree = () => {
-  return(
-    <div className="section-2-container live-stream-container"> 
+  const [dayOne, setDayOne] = useState(false);
+  const [dayTwo, setDayTwo] = useState(false);
+  const [dayThree, setDayThree]  = useState(false);
+
+  const toggleDayOne = () => setDayOne(!dayOne);
+  const toggleDayTwo = () => setDayTwo(!dayTwo);
+  const toggleDayThree = () => setDayThree(!dayThree);
+
+  const dayOneContent = () => {
+    return(
+      <div>
+        <h1> I am day 1 yaaaaaall</h1>
+      </div>
+    )
+  }
+
+  const dayTwoContent = () => {
+    return(
+      <div>
+        <h1> I am day 2 yaaaaaall</h1>
+      </div>
+    )
+  }
   
+  const dayThreeContent = () => {
+    return(
+      <div>
+        <h1> I am day 3 yaaaaaall</h1>
+      </div>
+    )
+  }
+
+  const renderContent = () => {
+    if(dayOne === true) {
+      return(
+        dayOneContent()
+      );
+    } else if (dayTwo === true) {
+      return(
+        dayTwoContent()
+      )
+    } else if(dayThree === true) {
+      return(
+        dayThreeContent()
+      )
+    } else {
+      return(
+        dayOneContent()
+      )
+    }
+  }
+
+  return(
+    <div className="section-3-container live-stream-container"> 
+      <div className="schedule-container"> 
+        <div className="button-container">
+          <button  onClick={toggleDayOne}> Day 18 </button>
+          <button  onClick={toggleDayTwo}> Day 19 </button>
+          <button  onClick={toggleDayThree}> Day 20</button>
+        </div>
+
+        {renderContent()}
+      </div>
     </div>     
   )
 };
