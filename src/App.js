f94501d76beea07c4100808f1b125e5bfb7d98eb
Itzamna Huerta <_@itzamna-huerta.local>
2020-12-16 19:37:27 -0600

    removed landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,8 +8,7 @@ import {
 } from "react-router-dom";
 
 // current main pages
-
-import LandingPage from './components/LandingPage';
+// import LandingPage from './components/LandingPage';
 import HomeScreen  from './components/HomeScreen';
 import AboutScreen from './components/AboutScreen';
 import NavBar from './components/NavBar';
@@ -17,22 +17,7 @@ import Shop from './components/Shop';
 
 import OjoDeAguaFilmFestival2020Screen from './components/OjoDeAguaFilmFestival2020/OjoDeAguaFilmFestival2020Screen.jsx';
 
-
 function App() {
-  const [landingPage, setLandingPage] = useState(true);
-
-  const toggleLandingPage = () => setLandingPage(!landingPage);
-
-  if(landingPage === true) {
-    return(
-      <React.Fragment> 
-        <LandingPage
-          landingPage={landingPage}
-          toggleLandingPage={toggleLandingPage}
-        />
-      </React.Fragment> 
-    )
-  } else {
     return (
       <div className="App">
         <Router>
@@ -51,7 +36,8 @@ function App() {
         </Router>
       </div>
     );
-  }
+
+
 }
 
 export default App;
